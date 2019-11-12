7bf934f0067508377fcc499dc90ae7f2dce22e7f
frodriguez <frodriguez@solati.com.co>
2019-11-12 10:48:53 -0500

    modulo mantenimiento

---
+++ app/Http/Controllers/Mantenimiento/AdministracionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Mantenimiento;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdministracionController extends Controller
+{
+    //verifica que el usuario tenga session iniciada para acceder
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function moduloAdministracion()
+    {
+       return view('mantenimiento/index');
+    }
+}
